ea5608b0f70e3cc6a48653c38cabaa49fcf22a22
Masa Utkovic <masa.utkovic@gmail.com>
2025-01-19 14:04:28 +0100

    feat: create home component

---
+++ src/Home/Home.tsx
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <div className="h-screen bg-gray-100 flex items-end">
+            <div className="w-full text-center pb-8">
+                <h1
+                    className="font-bold font-smooch text-black lowercase text-[15vw] tracking-[2.6vw]"
+                >
+                    tuffz.com
+                    <span
+                        className="text-[5vw] align-super ml-[1vw]"
+                    >
+                        &#169;
+                    </span>
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
+
